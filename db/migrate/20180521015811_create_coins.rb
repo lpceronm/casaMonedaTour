fd08ae4e85339c5aed2c4bd4814ee198aabe7cd3
Laura Ceron <lpceronm@slabcode.com>
2018-05-20 21:12:11 -0500

    Coins Scaffold

---
+++ db/migrate/20180521015811_create_coins.rb
@@ -0,0 +1,19 @@
+class CreateCoins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :coins do |t|
+      t.string :name
+      t.string :num_record
+      t.string :material
+      t.string :country
+      t.string :denomination
+      t.string :batch
+      t.text :location
+      t.string :issuer
+      t.string :serial_number
+      t.integer :part_number
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end

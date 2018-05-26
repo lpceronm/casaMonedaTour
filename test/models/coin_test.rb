742fa5b74c4c8ca196365a7516a0472bb02a196e
Laura Ceron <lpceronm@slabcode.com>
2018-05-26 13:02:21 -0500

    annotated model

--- test/models/coin_test.rb
+++ test/models/coin_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: coins
+#
+#  id            :bigint(8)        not null, primary key
+#  name          :string
+#  num_record    :string
+#  material      :string
+#  country       :string
+#  denomination  :string
+#  batch         :string
+#  location      :text
+#  issuer        :string
+#  serial_number :string
+#  part_number   :integer
+#  quantity      :integer
+#  image         :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'test_helper'
 
 class CoinTest < ActiveSupport::TestCase

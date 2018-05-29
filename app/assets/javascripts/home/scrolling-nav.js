dde138a8176bc825b2a65a182ad08412459dc38e
Laura Ceron <lpceronm@slabcode.com>
2018-05-29 11:17:25 -0500

    Fixes on JS

--- app/assets/javascripts/home/scrolling-nav.js
+++ app/assets/javascripts/home/scrolling-nav.js
@@ -1,5 +1,5 @@
 //jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+$(document).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
